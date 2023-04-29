d78e0316fe47977acafc413c1520e3369207c37d
Felipe Ferreira <felipe.diogenes13@gmail.com>
2023-04-29 14:43:06 -0300

    Initial Rails + React setup

---
+++ app/javascript/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default () => (
+    <div className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center">
+        <div className="jumbotron jumbotron-fluid bg-transparent">
+            <div className="container secondary-color">
+                <h1 className="display-4">Products</h1>
+                <p className="lead">
+                    List of Products
+                </p>
+            </div>
+        </div>
+    </div>
+);
