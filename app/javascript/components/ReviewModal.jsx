eafb0f8e7d69391f286aea2eb5d634f24d950221
Felipe Ferreira <felipe.diogenes13@gmail.com>
2023-04-29 23:04:04 -0300

    Improvements to the AddReview Modal

--- app/javascript/components/ReviewModal.jsx
+++ app/javascript/components/ReviewModal.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import Modal from 'react-bootstrap/Modal';
-// import { FaStar } from 'react-icons/fa';
+import { FaStar } from 'react-icons/fa';
 
 const ReviewModal = ({product, handleCloseModal}) => {
     const [rating, setRating] = useState(0);
@@ -29,64 +29,62 @@ const ReviewModal = ({product, handleCloseModal}) => {
                 <Modal.Title>{product.name}</Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                <div className="row">
-                    <div className="col-md-6">
-                        <img
-                            src={product.thumbnail}
-                            className="card-img-top"
-                            alt={`${product.name} image`}
-                        />
-                        <h5>{product.name}</h5>
-                    </div>
-                    <div className="col-md-6">
-                        <p>{product.description}</p>
+            <div className="row">
+                <div className="col-md-6 mb-1">
+                    <img
+                        src={product.thumbnail}
+                        className="card-img-top mb-1"
+                        alt={`${product.name} image`}
+                    />
+                    <h5>{product.name}</h5>
+                </div>
+                <div className="col-md-11">
+                    <p>{product.description}</p>
+                </div>
+            </div>
+            <div className="col-md-11">
+                <div className="form-group">
+                    <label htmlFor="userName">Your Name:</label>
+                    <input
+                        type="text"
+                        id="userName"
+                        value={userName}
+                        onChange={(e) => setUserName(e.target.value)}
+                        className="form-control mb-3"
+                    />
+                </div>
+                <div className="form-group mb-3">
+                    <label>Rating:</label>
+                    <div>
+                        {[...Array(5)].map((star, i) => {
+                            const ratingValue = i + 1;
+                            return (
+                                <FaStar
+                                    key={i}
+                                    className="star"
+                                    color={ratingValue <= rating ? '#ffc107' : '#e4e5e9'}
+                                    size={25}
+                                    onClick={() => handleRatingClick(ratingValue)}
+                                />
+                            );
+                        })}
                     </div>
                 </div>
-                <div className="row">
-                    <div className="col-md-6">
-                        <label htmlFor="userName">Your Name:</label>
-                        <input
-                            type="text"
-                            id="userName"
-                            value={userName}
-                            onChange={(e) => setUserName(e.target.value)}
-                            className="form-control mb-2"
-                        />
-                    </div>
-                    <div className="col-md-6">
-                        <label>Rating:</label>
-                        <div>
-                            {[...Array(5)].map((star, i) => {
-                                const ratingValue = i + 1;
-                                return (
-                                    <div key={i}> test </div>
-                                    // <FaStar
-                                    //     key={i}
-                                    //     className="star"
-                                    //     color={ratingValue <= rating ? '#ffc107' : '#e4e5e9'}
-                                    //     size={25}
-                                    //     onClick={() => handleRatingClick(ratingValue)}
-                                    // />
-                                );
-                            })}
-                        </div>
-                    </div>
+            </div>
+            <div className="row">
+                <div className="col-md-12">
+                    <label htmlFor="reviewText">Your Review:</label>
+                    <textarea
+                        id="reviewText"
+                        value={reviewText}
+                        onChange={(e) => setReviewText(e.target.value)}
+                        className="form-control mb-2"
+                    />
                 </div>
-                <div className="row">
-                    <div className="col-md-12">
-                        <label htmlFor="reviewText">Your Review:</label>
-                        <textarea
-                            id="reviewText"
-                            value={reviewText}
-                            onChange={(e) => setReviewText(e.target.value)}
-                            className="form-control mb-2"
-                        />
-                    </div>
-                </div>
+            </div>
             </Modal.Body>
             <Modal.Footer>
-                <button onClick={handleCloseModal}>Cancel</button>
-                <button onClick={handleSubmitReview}>Submit Review</button>
+                <button onClick={handleSubmitReview}>Add Review</button>
             </Modal.Footer>
         </Modal>
     );
